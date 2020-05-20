df7d540a053736842b84be56f8a78037e9b75e05
wcf@blibao.cn <19821122>
2020-05-20 23:53:39 +0800

    netty实现rpc远程调用。

--- src/main/java/com/aote/netty/rpc/customer/ClientBootstrap.java
+++ src/main/java/com/aote/netty/rpc/customer/ClientBootstrap.java
@@ -13,16 +13,19 @@ public class ClientBootstrap {
     // 定义协议头
     public static final String providerName = "HelloService#hello#";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 创建消费者
         NettyClient customer = new NettyClient();
 
         //创建代理对象
         HelloService service = (HelloService) customer.getBean(HelloService.class,providerName);
 
-        // 写个循环间隔调用
-        String res = service.hello("你好 dubbo~~~");
-        System.out.println("调用结果 res="+res);
+        for (;; ) {
+            Thread.sleep(2 * 1000);
+            //通过代理对象调用服务提供者的方法(服务)
+            String res = service.hello("你好 dubbo~~~");
+            System.out.println("调用的结果 res= " + res);
+        }
 
     }
 
--- src/main/java/com/aote/netty/rpc/provide/HelloServiceImpl.java
+++ src/main/java/com/aote/netty/rpc/provide/HelloServiceImpl.java
@@ -16,8 +16,8 @@ public class HelloServiceImpl implements HelloService {
         System.out.println("收到客户端消息=" + msg);
         //根据mes 返回不同的结果
         if(msg != null) {
-            return "你好客户端, 我已经收到你的消息 [" + msg + "] ";
-//            return "你好客户端, 我已经收到你的消息 [" + msg + "] 第" + (++count) + " 次";
+//            return "你好客户端, 我已经收到你的消息 [" + msg + "] ";
+            return "你好客户端, 我已经收到你的消息 [" + msg + "] 第" + (++count) + " 次";
         } else {
             return "你好客户端, 我已经收到你的消息 ";
         }
